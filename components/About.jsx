ef157d4fdbf5fa1c0bf182a9cccfcd6e325813c8
Agsslmmmmm <111561779+Agsslmmmmm@users.noreply.github.com>
2023-06-04 04:16:07 +0700

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
             <div className='col-span-2 justify-center'>
                 <p className='text-xl uppercase tracking-widest text-[#1f6d7d]'>ABOUT</p>
                 <h2 className='text-xl font-bold py-4'>Who I Am</h2>
-                <p className='py-2 text-gray-600'>// I am Agus Salim</p>
+                <p className='py-2 text-gray-600'>I am Agus Salim</p>
                 <p className='py-2 text-gray-600'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt praesentium sit doloremque cumque eos aliquid esse? Quasi, laudantium hic laborum soluta aperiam, exercitationem non corporis odit at, dolores nisi ipsum? Ullam dicta quisquam officia tempora vel! Fugit, corrupti blanditiis repudiandae omnis delectus fugiat sint hic nesciunt vero quas temporibus doloremque?</p>
                 <p className='py-2 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quasi, eos magnam assumenda aspernatur placeat. Accusamus recusandae asperiores numquam excepturi in illum eos blanditiis quis, aut ea et ab aliquid error atque explicabo beatae sapiente quos magnam libero ad accusantium commodi omnis. Accusantium optio aspernatur non blanditiis amet, omnis libero error, possimus voluptatum quis tempora.</p>
                 <p className='py-2 text-blue-600 underline cursor-pointer'>Check out some of my projects.</p>
