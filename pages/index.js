d7500aad96cdbbc909113f00ea5e2a46e5b7d9c2
Agsslmmmmm <111561779+Agsslmmmmm@users.noreply.github.com>
2023-09-25 09:50:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,52 @@
-import Image from 'next/image'
-import Head from 'next/head'
-import { Navbar } from '@/components/Navbar'
-import Main from '../components/Main'
-import About from '@/components/About'
-import Projects from '@/components/Projects'
-import Skills from '@/components/Skills'
-import {useRef} from 'react'
+import React, { useState, useEffect } from 'react';
 
+const Navbar = () => {
+    const [isActive, setIsActive] = useState(false);
 
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
 
-export default function Home() {
-  return (
-   <div>
-    <Head>
-      <title>Agus | Front-End Developer</title>
-      <meta name="description" content='Generate by create next app' />
-      <link rel="icon" href="/public/assets/pr.jpg" />
-    </Head>
+    useEffect(() => {
+        // Fungsi untuk mendeteksi bagian yang sedang aktif
+        const handleScroll = () => {
+            const sections = document.querySelectorAll("section");
+            sections.forEach((section) => {
+                const rect = section.getBoundingClientRect();
+                if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                    // Bagian ini sedang aktif, Anda dapat mengganti warna latar belakang Navbar sesuai kebutuhan
+                    setIsActive(true);
+                } else {
+                    // Bagian ini tidak aktif
+                    setIsActive(false);
+                }
+            });
+        };
 
-    
-    <Navbar />
+        // Menambahkan event listener untuk mendeteksi perubahan posisi scroll
+        window.addEventListener("scroll", handleScroll);
 
-    <div className="header">
+        // Membersihkan event listener saat komponen Navbar tidak lagi digunakan
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-    <Main />
-    </div>
-    <div className="about">
+    return (
+        <div className='fixed top-5 left-0 right-0'>
 
-    <About />
-    </div>
-    <div className="skills">
+            <nav
+                class="border border-b rounded-full p-4 shadow-lg hover:scale-110 transition-transform duration-500 w-[90%] mx-auto scrolling-navbar bg-white">
+                <ul class="flex space-x-4 items-center justify-center">
+                    <li className='hover:scale-110 transition-transform duration-500'><a href="#home" class="nav-link uppercase text-gray-400">Home</a></li>
+                    <li className='hover:scale-110 transition-transform duration-500'><a href="#about" class="nav-link uppercase text-gray-400">About</a></li>
+                    <li className='hover:scale-110 transition-transform duration-500'><a href="#team" class="nav-link uppercase text-gray-400">Skills</a></li>
+                    <li className='hover:scale-110 transition-transform duration-500'><a href="#contact" class="nav-link uppercase text-gray-400">Projects</a></li>
+                </ul>
+            </nav>
+        </div>
 
-    <Skills />
-    </div>
-    <div className="projects">
+    );
+};
 
-    <Projects />
-    </div>
-
-    
-   </div>
-  )
-}
+export default Navbar;
